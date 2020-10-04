e2d51acb436b7726101a34ce467f6827764329c3
Tanapat <tanapat.sri@ku.th>
2020-10-05 00:20:04 +0700

    bankaccount

---
+++ src/main/java/th/ac/ku/backaccount/controller/BankAccountRestController.java
@@ -0,0 +1,61 @@
+package th.ac.ku.backaccount.controller;
+
+import org.springframework.web.bind.annotation.*;
+import th.ac.ku.backaccount.data.BankAccountRepository;
+import th.ac.ku.backaccount.model.BankAccount;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bankaccount")
+public class BankAccountRestController {
+
+    private BankAccountRepository repository;
+
+    public BankAccountRestController(BankAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public List<BankAccount> getAllCustomerId(@PathVariable int customerId) {
+        return repository.findByCustomerId(customerId);
+    }
+
+
+    @GetMapping
+    public List<BankAccount> getall() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BankAccount getOne(@PathVariable int id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public BankAccount create(@RequestBody BankAccount bankAccount) {
+        BankAccount record = repository.save(bankAccount);
+        repository.flush();
+        return record;
+    }
+
+    @PutMapping("/{id}")
+    public BankAccount update(@PathVariable int id,
+                              @RequestBody BankAccount bankAccount) {
+        BankAccount record = repository.findById(id).get();
+        record.setBalance(bankAccount.getBalance());
+        repository.save(record);
+        return record;
+    }
+
+    @DeleteMapping("/{id}")
+    public BankAccount delete(@PathVariable int id) {
+        BankAccount record = repository.findById(id).get();
+        repository.deleteById(id);
+        return record;
+    }
+
+
+
+
+}
